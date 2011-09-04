2fd502cf89bb157fc5f5ae929e1bf27675c4460d
roxlu <diederick@apollomedia.nl>
2011-09-04 13:25:30 +0200

    Added DictionaryMap instead of declaration in the same header

--- src/DictionaryMap.h
+++ src/DictionaryMap.h
@@ -3,23 +3,20 @@
 
 #include <map>
 #include <string>
-#include "Dictionary.h"
 
 using std::map;
+using std::string;
+
+
+class Dictionary;
 struct DictionaryMap {
+	DictionaryMap(DictionaryMap& other);
+	DictionaryMap(); 
+	
 	string type_name;
 	map<string, Dictionary> children;
 	bool is_array;
 	
-	DictionaryMap(DictionaryMap& other) {
-		type_name = other.type_name;
-		children = other.children;
-		is_array = other.is_array;
-	}	
-	
-	DictionaryMap() {
-		is_array = false;
-	}
 };
 
 #endif
