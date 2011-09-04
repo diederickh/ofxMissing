f62506d00518851e0851df948eafabe0a9366065
roxlu <diederick@apollomedia.nl>
2011-09-04 13:58:22 +0200

    Fixes for windows mingw

--- src/Date.cpp
+++
@@ -1,8 +0,0 @@
-#include "Date.h"
-
-// returns something like: 2011.06.19.22.31
-string getDateTimeAsString(string sFormat) {
-	Poco::LocalDateTime now;
-	string str = DateTimeFormatter::format(now, sFormat);	
-	return str;
-};
